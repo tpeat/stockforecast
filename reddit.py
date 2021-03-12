5c060cf7d74125f321c179a7af0ea255dbc6b870
tpeat <65079595+tpeat@users.noreply.github.com>
2021-03-12 10:40:24 -0500

    words by ticker

--- reddit.py
+++ reddit.py
@@ -1,13 +1,23 @@
 from psaw import PushshiftAPI
 import pandas as pd
 import datetime
+import numpy as np
 
 api = PushshiftAPI()
 
 start_time = int(datetime.datetime(2021,3,12).timestamp())
-content = list(api.search_submissions(after=start_time,subreddit='wallstreetbets', filter=['url', 'author','title','subreddit'],limit=10))
+content = api.search_submissions(after=start_time,
+                                 subreddit='wallstreetbets',
+                                 filter=['url', 'author','title','subreddit'])
 
+df = pd.DataFrame(content)
 for post in content:
-  print(post.created_utc)
-  print(post.title)
-  print(post.url)
+    words = post.title.split()
+    cashtags = list(set(filter(lambda word: word.lower().startswith('$'), words)))
+    if len(cashtags) > 0:
+      print(cashtags)
+      print(content.title)
+ # print(post.created_utc)
+ # print(post.title)
+ # print(post.url)
+
